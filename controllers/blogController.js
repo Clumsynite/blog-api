c5bf8057f35fd82a94615b499eba2e6b575b9886
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 18:01:05 +0530

    added controller to read blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,3 +1,12 @@
 const User = require("../models/user");
 const Blog = require("../models/blog");
 const Comment = require("../models/comment");
+
+exports.blog_get = (req, res, next) => {
+  Blog.find({}, (err, data) => {
+    if (err) {
+      return res.sendStatus(404);
+    }
+    res.send(data);
+  });
+};
