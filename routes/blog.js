b036d122b6ac7afbcf5a411b0b3abfec0560e7d6
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 22:58:13 +0530

    added route for new blogs

--- routes/blog.js
+++ routes/blog.js
@@ -5,5 +5,5 @@ const blog = require("../controllers/blogController");
 
 router.get("/all", blog.blog_get);
 router.get("/:id/view", blog.user_blogs_get);
-
+router.post("/new", blog.new_post)
 module.exports = router;
