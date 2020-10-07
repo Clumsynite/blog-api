9f3934cb4ef6b1f1f1a3c5aa7c800f5eb51c4883
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 23:27:40 +0530

    added route to get user's drafts

--- routes/blog.js
+++ routes/blog.js
@@ -6,4 +6,6 @@ const blog = require("../controllers/blogController");
 router.get("/all", blog.blog_get);
 router.get("/:id/view", blog.user_blogs_get);
 router.post("/new", blog.new_post);
+router.get("/:id/drafts", blog.user_drafts_get);
+
 module.exports = router;
