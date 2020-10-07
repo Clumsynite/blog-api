ea3f9d211c5ea16d45255d269876646fc27c07eb
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 21:29:48 +0530

    added route for my profile and changed user profile route

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ const user = require("../controllers/userController");
 
 router.post("/signup", user.signup_post);
 router.put("/:id/update", user.user_update_post);
-router.get("/:id", user.user_get);
-
+router.get("/:id/view", user.user_get);
+router.get('/me', user.profile_get)
 module.exports = router;
