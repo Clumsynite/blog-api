fb6ecc01b4e2a2eee4d1957a9e763d7ef96c3e72
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 18:00:19 +0530

    added routing for blogs, login and signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const blog = require('../controllers/blogController');
-const user = require('../controllers/userController');
-const comment = require('../controllers/commentController');
+const blog = require("../controllers/blogController");
+const user = require("../controllers/userController");
+const comment = require("../controllers/commentController");
 
+// Blog
+router.get("/blogs", blog.blog_get);
 
-module.exports = router
+// User
+router.post("/signup", user.signup_post);
+router.post("/login", user.login_post);
+
+// Comment
+
+module.exports = router;
