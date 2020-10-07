7ea9900618710dba1d57dfcf10f9a84c389d6c42
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-08 00:16:49 +0530

    added route for new comment and view blog

--- routes/blog.js
+++ routes/blog.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const blog = require("../controllers/blogController");
+const { route } = require("./user");
 
 router.get("/all", blog.blog_get);
+router.get("/:id/view", blog.blog_view_get)
 router.post("/new", blog.new_post);
 router.put("/:id/update", blog.update_put);
 router.delete("/:id/delete", blog.blog_delete);
+router.post("/:id/comment/new", blog.comment_new_post)
 
 module.exports = router;
