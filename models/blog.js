ba7ac52bfe851a4cc46a781650650b5a42635937
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 17:59:27 +0530

    added moment library to format timestamp

--- models/blog.js
+++ models/blog.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const moment = require('moment');
 
 const BlogSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User" },
