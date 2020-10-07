7326476f6e19cbb2835047251744c4633f2000a7
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-07 15:12:44 +0530

    Created Blog model

---
+++ models/blog.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  added: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Blog", BlogSchema);
