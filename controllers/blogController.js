69b7fe7e1d71457368430fef70e8ad73e03c3995
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-08 00:15:39 +0530

    added new comment and blog view control

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -11,6 +11,16 @@ exports.blog_get = (req, res, next) => {
   });
 };
 
+exports.blog_view_get = async(req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+    const comment = await Comment.find({blog: req.params.id})
+    res.json({blog, comment})
+  } catch (error) {
+    res.status(404).json({ error: error })
+  }
+}
+
 exports.new_post = async (req, res) => {
   try {
     const blog = await new Blog({
@@ -45,6 +55,20 @@ exports.blog_delete = async (req, res) => {
   try {
     await Blog.findByIdAndRemove(req.params.id);
     res.json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
+
+exports.comment_new_post = async (req, res) => {
+  try {
+    const comment = await new Comment({
+      author: req.user._id,
+      blog: req.params.id,
+      title: req.body.title,
+      content: req.body.content
+    }).save()
+    res.json(comment)
   } catch (error) {
     res.status(404).json({ error: error });
   }
