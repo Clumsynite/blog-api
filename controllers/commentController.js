1cb015d684e79fa9a8115b1461ef0bfdcd5c80cc
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-08 16:24:29 +0530

    refactored and added comment update route an control

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,11 +2,35 @@ const User = require("../models/user");
 const Blog = require("../models/blog");
 const Comment = require("../models/comment");
 
-exports.comment_get = async(req, res) => {
+exports.comment_get = async (req, res) => {
   try {
-    const comment = await Comment.findById(req.params.id)
-    res.json(comment)
+    const comment = await Comment.findById(req.params.id);
+    res.json(comment);
   } catch (error) {
-    res.status(404).json({ error: error })
+    res.status(404).json({ error: error });
   }
-}
+};
+
+exports.comment_update = async (req, res) => {
+  try {
+    const owner = await Comment.find({
+      _id: req.params.id,
+      author: req.user._id,
+    });
+    if (owner.length > 0) {
+      const comment = {
+        _id: req.params.id,
+        blog: owner[0].blog,
+        author: req.user._id,
+        title: req.body.title,
+        content: req.body.content,
+      };
+      await Comment.findByIdAndUpdate(req.params.id, comment, {});
+      res.json(comment);
+    } else {
+      res.json({ error: "You can't edit someone else's comment" });
+    }
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
--- routes/comment.js
+++ routes/comment.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const comment = require("../controllers/commentController");
 
-router.get('/:id/view', comment.comment_get)
+router.get("/:id/view", comment.comment_get);
+router.put("/:id/update", comment.comment_update);
 
 module.exports = router;
