07c83fc8e8cdd309afddfe3e883c88b71199d719
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-08 15:50:24 +0530

    refactored

--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,6 @@ router.get("/:id/view", user.user_get);
 router.get("/me", user.profile_get);
 router.get("/:id/blogs", user.blog_get);
 router.get("/drafts", user.drafts);
-router.get("/:id/comments", user.comment_get)
+router.get("/:id/comments", user.comment_get);
+
 module.exports = router;
