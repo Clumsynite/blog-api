719f4881ca5b03170e28477f9f216d0975c89495
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-08 16:34:51 +0530

    added delete comment route

--- routes/comment.js
+++ routes/comment.js
@@ -5,5 +5,6 @@ const comment = require("../controllers/commentController");
 
 router.get("/:id/view", comment.comment_get);
 router.put("/:id/update", comment.comment_update);
+router.delete("/:id/delete", comment.comment_delete);
 
 module.exports = router;
