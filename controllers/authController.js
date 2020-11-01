cf85364ff162531d292c99955777688fce7ead6b
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-01 16:09:26 +0530

    removed cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,11 +18,7 @@ exports.login_post = (req, res) => {
         firstname: user.firstname,
       };
       const token = jwt.sign({ user: body }, process.env.JWT_SECRET);
-      res.cookie("auth", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-      });
+      res.cookie("auth", token);
       res.json({ user, token });
     });
   })(req, res);
